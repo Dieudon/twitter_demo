fc51ccc978976e4a6a64c3592569fc0bbba6edd7
Dieudon <d.egstorf@publitas.com>
2013-08-20 11:01:15 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterDemo::Application.config.secret_token = '170324615d1ae1fb495b21624eaa0c104980d5983be5384eb0bdc127ed9d011125030a99fc27c99c0b734fe64591f4cc035bf22de14914b92c4696699493e62d'
